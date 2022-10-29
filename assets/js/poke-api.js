f8e2aab2d73ad77e4c08bec6733adba51b04962c
Gabriel Ferreira do Prado <thegaprado@gmail.com>
2022-10-29 07:37:08 -0300

    Converting PokeApi model to Pokemon Class

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,12 +2,37 @@
 
 const pokeApi = {};
 
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-           .then((response) => response.json());
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    
+    let num = pokeDetail.id;
+    if (num < 10) {
+        pokemon.number = '00' + num;
+    } else if (num < 100) {
+        pokemon.number = '0' + num;
+    } else {
+        pokemon.number = num;
+    }
+    
+    pokemon.name = pokeDetail.name;
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types;
+    pokemon.types = types;
+    pokemon.type = type;
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+    return pokemon;
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+           .then((response) => response.json())
+           .then(convertPokeApiDetailToPokemon);
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 32) => {
 
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
